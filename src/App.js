82d8712e62ae4527b4dc8c16369b81bb6969ac12
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-10 18:39:51 +0430

    created the login page and give it style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+//Components
+import Login from './components/Login';
+
+
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div>
+      <Switch>
+        <Route path="/" component={Login}/>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
